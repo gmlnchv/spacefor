bf042227717462a0bc27188ad2e20642cea989da
Georgy Malanichev <georgy@malanichev.com>
2024-02-19 22:06:04 +1300

    Update hero component styling

--- apps/remix/app/components/hero.tsx
+++ apps/remix/app/components/hero.tsx
@@ -6,11 +6,9 @@ const Hero = ({ title, description, image }: HeroProps) => {
   return (
     <section className="py-10 md:py-24 max-h-[875px]">
       <Container>
-        <div className="flex max-sm:flex-wrap items-center gap-6 justify-between">
-          <header className="space-y-6 lg:space-y-14 text-balance md:w-1/2 xl:w-auto max-w-[660px]">
-            <h1 className="text-4xl md:text-6xl xl:text-8xl text-balance">
-              {title}
-            </h1>
+        <div className="flex max-sm:flex-wrap items-center gap-8 justify-between">
+          <header className="space-y-8 lg:space-y-14 text-balance md:w-1/2 xl:w-auto max-w-[660px]">
+            <h1 className="text-5xl xl:text-8xl">{title}</h1>
             <p className="text-balance">{description}</p>
           </header>
 
